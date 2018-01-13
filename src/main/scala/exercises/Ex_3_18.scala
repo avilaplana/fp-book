dff6c09a8a43563cf88a74bac0ceaf945c1c0c2c
Alvaro Vilaplana <alvaro.vilaplana@gmail.com>
2018-01-13 18:16:14 +0000

    3_18 and 3_19

--- src/main/scala/exercises/Ex_3_18.scala
+++ src/main/scala/exercises/Ex_3_18.scala
@@ -12,9 +12,9 @@ object Ex_3_18 {
 
   object List {
 
-    def map[A, B](source: List[A], f: A => B): List[B] = source match {
+    def map[A, B](source: List[A])(f: A => B): List[B] = source match {
       case Nil => Nil
-      case Cons(x, xs) => Cons(f(x), map(xs, f))
+      case Cons(x, xs) => Cons(f(x), map(xs)(f))
     }
 
     def apply[A](as: A*): List[A] = if (as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
@@ -23,9 +23,9 @@ object Ex_3_18 {
   def main(args: Array[String]): Unit = {
     import List._
 
-    assert(map[Int, String](List(1, 2), x => x.toString) == List("1", "2"))
-    assert(map[Int, Int](List(1, 2, 1, 3), x => x + 1) == List(2, 3, 2, 4))
-    assert(map[Int, Int](Nil, x => x + 1) == Nil)
+    assert(map(List(1, 2))(x => x.toString) == List("1", "2"))
+    assert(map(List(1, 2, 1, 3))(x => x + 1) == List(2, 3, 2, 4))
+    assert(map[Int, Int](Nil)(x => x + 1) == Nil)
     // todo type inference for higher-order functions in this version of scala?
   }
 }
