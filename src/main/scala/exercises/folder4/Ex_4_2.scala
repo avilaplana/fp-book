64471eb39b2187a52bc9e5b6dd5235f00a9034e3
Alvaro Vilaplana <alvaro.vilaplana@gmail.com>
2018-01-16 22:32:59 +0000

    4_2

---
+++ src/main/scala/exercises/folder4/Ex_4_2.scala
@@ -0,0 +1,71 @@
+package exercises.folder4
+
+import com.sun.javaws.exceptions.InvalidArgumentException
+
+//Implement the variance function in terms of flatMap. If the mean of a sequence is m, the variance is the mean of
+// math.pow(x - m, 2) for each element x in the sequence
+object Ex_4_2 {
+
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = this match {
+      case None => None
+      case Some(a) => f(a)
+    }
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(v) => v
+    }
+
+    def orElse[B >: A](default: => Option[B]): Option[B] = this match {
+      case None => default
+      case Some(v) => Some(v)
+    }
+
+    def filter(f: A => Boolean): Option[A] = this match {
+      case None => None
+      case Some(v) if f(v) => Some(v)
+      case _ => None
+    }
+
+    def get: A = this match {
+      case None => throw new RuntimeException("there is no value")
+      case Some(v) => v
+    }
+  }
+
+
+  object Option {
+    def variance(xs: Seq[Double]): Option[Double] = {
+      val mean: Seq[Double] => Double = s => s.sum / s.length
+      xs match {
+        case Nil => None
+        case s@_ =>
+          val m = mean(s)
+          Some(mean(s.map(x => math.pow(x - m, 2))))
+      }
+    }
+  }
+
+
+  case object None extends Option[Nothing]
+
+  case class Some[A](value: A) extends Option[A] {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    import Option._
+
+    def truncate(d: Double): Double = (math floor d * 100)/100
+
+    assert(truncate(variance(Seq(1, 4, 5, 7, 8, 10)).get) == 8.47D)
+    assert(truncate(variance(Seq(1, 2, 5, 9, 10, 12)).get) == 16.91D)
+    assert(variance(Nil) == None)
+  }
+}
