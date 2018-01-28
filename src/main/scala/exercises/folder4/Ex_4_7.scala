98f8eac5055699aebce3449163fc9196a37d781d
Alvaro Vilaplana <alvaro.vilaplana@gmail.com>
2018-01-28 16:34:38 +0000

    4_7

--- src/main/scala/exercises/folder4/Ex_4_7.scala
+++ src/main/scala/exercises/folder4/Ex_4_7.scala
@@ -31,10 +31,27 @@ object Ex_4_7 {
       (a, b) => a.flatMap(aa => b.map(bb => aa :: bb))
     }
 
+    def sequence_1[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es.foldRight[Either[E, List[A]]](Right(List.empty[A])) {
+      (a, b) =>
+        for {
+          aa <- a
+          bb <- b
+        } yield aa :: bb
+    }
+
     def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
-        as.foldRight[Either[E, List[B]]](Right(List.empty[B])) {
-          (a,b) => f(a).flatMap(aa => b.map(bb => aa :: bb))
-        }
+      as.foldRight[Either[E, List[B]]](Right(List.empty[B])) {
+        (a, b) => f(a).flatMap(aa => b.map(bb => aa :: bb))
+      }
+
+    def traverse_1[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+      as.foldRight[Either[E, List[B]]](Right(List.empty[B])) {
+        (a, b) =>
+          for {
+            aa <- f(a)
+            bb <- b
+          } yield aa :: bb
+      }
   }
 
   def main(args: Array[String]): Unit = {
@@ -43,12 +60,23 @@ object Ex_4_7 {
     assert(sequence[String, Int](List(Right(1), Right(2), Right(3))) == Right(List(1, 2, 3)))
     assert(sequence[String, Int](List(Right(1), Left("SOME_ERROR"), Right(3))) == Left("SOME_ERROR"))
 
+    assert(sequence_1[String, Int](List(Right(1), Right(2), Right(3))) == Right(List(1, 2, 3)))
+    assert(sequence_1[String, Int](List(Right(1), Left("SOME_ERROR"), Right(3))) == Left("SOME_ERROR"))
+
     assert(
       traverse[String, Int, Boolean](List(1, 3, 5, 7, 9))(i => if (i % 2 == 0) Left("Even found") else Right(true)) == Right(List(true, true, true, true, true))
     )
 
     assert(
       traverse[String, Int, Boolean](List(1, 2, 5, 7, 9))(i => if (i % 2 == 0) Left("Even found") else Right(true)) == Left("Even found")
+    )
+
+    assert(
+      traverse_1[String, Int, Boolean](List(1, 3, 5, 7, 9))(i => if (i % 2 == 0) Left("Even found") else Right(true)) == Right(List(true, true, true, true, true))
+    )
+
+    assert(
+      traverse_1[String, Int, Boolean](List(1, 2, 5, 7, 9))(i => if (i % 2 == 0) Left("Even found") else Right(true)) == Left("Even found")
     )
 
   }
