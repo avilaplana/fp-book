a4d1ad60e8ec649c1999e595e076877019da9b9f
Alvaro Vilaplana <alvaro.vilaplana@gmail.com>
2018-02-09 16:32:49 +0000

    Ex_6_1

---
+++ src/main/scala/exercises/chapter6/Ex_6_1.scala
@@ -0,0 +1,33 @@
+package exercises.chapter6
+
+object Ex_6_1 {
+
+  trait RNG {
+    def nextInt: (Int, RNG)
+  }
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL * 0XBL) & 0XFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    rng.nextInt match {
+      case (i,r) if i >= 0 => (i,r)
+      case (i,r) if i == Int.MinValue => nonNegativeInt(r)
+      case (i,r) => (-i, r)
+    }
+  }
+
+
+  def main(args: Array[String]) = {
+    val (i1,r1) = nonNegativeInt(new SimpleRNG(100))
+    val (i2,r2) = nonNegativeInt(r1)
+    val (i3,r3) = nonNegativeInt(r2)
+    val (i4,r4) = nonNegativeInt(r3)
+  }
+}
